8fedd718f4910693f8425a5a2220f4de5ae22f35
Rohit Singh Rawat <rohitrawat4073@gmail.com>
2025-09-03 00:44:23 +0530

    add package

--- jobportal-backend/src/main/java/com/example/jobportal/security/JwtUtil.java
+++ jobportal-backend/src/main/java/com/example/jobportal/security/JwtUtil.java
@@ -1,3 +1,19 @@
+package com.example.jobportal.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
 @Component
 public class JwtUtil {
 
